46c3cf86abeac8984e8211f4260e8cadf94b8611
HeshamEldash <heshamaldash44@gmail.com>
2023-07-01 11:23:46 +0100

    created the jobSchema using Joi

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const Hapi = require("@hapi/hapi");
+const Joi = require("joi");
+
+// ## Job Entity
+
+//id: required, string. must be a valid UUID (v4).
+// type: required, string. must be one of 'ON_DEMAND', 'SHIFT' or 'SCHEDULED'.
+// priceInPence: required, integer. can be zero, but cannot be negative.
+
+// contactEmail: optional, string. must be a valid email if provided.
+// status: required, string. must be one of 'AVAILABLE', 'ASSIGNED' or 'COMPLETED'.
+
+// createdAt: required, string. must be a valid date in ISO 8601 format. automatically set when a Job entity is created.
+// updatedAt: optional, string. must be a valid date in ISO 8601 format. defaults to null, automatically set whenever a Job entity is updated.
+
+
+const jobs = []
+
+const jobSchema = Joi.object({
+  id: Joi.string().guid({
+    version: "uuidv4",
+  }),
+  type: Joi.string().valid("ON_DEMAND", "SHIFT", "SCHEDULED"),
+  priceInPence: Joi.number().positive(),
+  contactEmail: Joi.string().email().optional(),
+  status: Joi.string().valid("AVAILABLE", "ASSIGNED", "COMPLETED"),
+  createdAt: Joi.date().iso(),
+  updatedAt: Joi.date().iso().optional().default(null)
+});
+
+
 
 const server = Hapi.server({
   port: 3000,
   host: "localhost",
 });
 
-
-
-
 server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-
-        return 'Hello World!';
-    }
+  method: "GET",
+  path: "/",
+  handler: (request, h) => {
+    return "Hello World!";
+  },
 });
 
-
-
-
-
-
 const init = async () => {
-
   try {
     await server.start();
     console.log(`server running on ${server.info.uri}`);
@@ -33,7 +53,21 @@ const init = async () => {
   }
 };
 
-
-
-
 init();
+
+//TODO 1: **Endpoint**: `/jobs/{id} - GET`
+// This endpoint should return a single Job, matching by the ID specified as a route parameter.
+
+//TODO 2: ### Update Job by ID
+// **Endpoint**: `/jobs/{id} - PATCH`
+// This endpoint should parse incoming JSON data and update an existing Job entity, returning it in the response.
+// The incoming JSON payload should be as follows:
+// ```
+// contactEmail: optional, string. must be a valid email if provided.
+// status: required, string. must be one of 'AVAILABLE', 'ASSIGNED' or 'COMPLETED'.
+// ```
+// Only the `contactEmail` and `status` attributes can be updated. The `updatedAt` should be updated if an update takes place.
+
+//TODO 3 : ### Delete Job by ID
+// **Endpoint**: `/jobs/{id} - DELETE`
+// This endpoint should delete a Job from the in-memory store, matching by the ID specified as a route parameter.
